8d0625aef46e519e19adaeab30073a3ad3f7c52b
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-05 15:02:04 +0500

    header finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./html/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        kokimtir: "#0D0D2B", // Siz kiritgan rang
+        secondary: "#2B076E", // Qizil rang
+        btn: "#3671E9", // Yashil rang
+        sariq: "#FFFF00", // Sariq rang
+      },
+    },
   },
   plugins: [],
 };
